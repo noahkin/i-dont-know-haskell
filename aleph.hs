debb6882cb61716107c78bf79c483422fd68c8d4
Noah Kindervag <noahk@protonmail.com>
2018-08-24 22:57:15 -0500

    Initial commit

---
+++ aleph.hs
@@ -0,0 +1,59 @@
+head' :: [a] -> a
+head' [] = error "nah bro there isn't even like one thing in there dude"
+head' (x:_ ) = x
+
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort [x] = [x]
+mergesort (x:xs) = merge [x] (mergesort xs)
+
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge (x:xs) (y:ys)
+    | x <= y = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
+merge [] [] = []
+merge x [] = x
+merge [] y = y
+
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+
+f = [fib x | x <- [0,1..]]
+
+
+fib :: (Integral a) => a -> a
+fib 0 = 1
+fib 1 = 1
+fib n
+    | n >= 2 = (fib (n - 1)) + (fib (n - 2))
+    | otherwise = error "sounds like that's a negative number you got right there"
+
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' [x] = [x]
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x : filter' p xs
+    | otherwise = filter' p xs
+
+
+collatz :: Integer -> [Integer]
+collatz n
+    | n == 1 = [1]
+    | even n = n : collatz (div n 2)
+    | odd n = n : collatz (n*3+1)
