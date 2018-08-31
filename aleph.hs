36394c04855dcc90f936e370b3cd5011ca4e60ca
Noah Kindervag <noahk@protonmail.com>
2018-08-31 17:54:25 -0500

    Did a small amount of stuff.

--- aleph.hs
+++ aleph.hs
@@ -68,3 +68,6 @@ sum'' = foldl (+) 0
 
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
